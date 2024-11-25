9c790fc7e41cd748de6f1d91eed6d36325d5205d
Prothes Barai <164070520+developerprothes@users.noreply.github.com>
2024-11-25 15:30:23 +0600

    Add files via upload

---
+++ even_odd_checker.js
@@ -0,0 +1,10 @@
+const number = prompt("Enter a number: ");
+
+//check if the number is even
+if(number % 2 == 0) {
+    console.log(number,"the number is even.");
+}
+// if the number is odd
+else {
+    console.log(number,"the number is odd.");
+}
